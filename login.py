73112739d323946fc26adfea9e9bd65c2ee7dffd
mikechen <mikechen0708@163.com>
2020-01-10 16:28:11 +0800

    更新

---
+++ login.py
@@ -0,0 +1,34 @@
+# username = input('username:')
+# print("welcome "+ username)
+# print("welcome %s" % username)
+
+#input读入的内容一定是字符
+# n = input( 'number: ')
+# a =int(n)+10  #int函数可以将字符转换为数字换成整数
+# print(a)
+# b = n +str(10)  #str 函数可以将对象转换为字符串
+# print(b)
+
+# >>> 5 /3
+# 1.6666666666666667
+# >>> 5//3 #得到商
+# 1
+# >>> divmod(5,3) #得到商和余数
+# (1, 2)
+# >>> a,b = divmod(5,3)
+# >>> a
+# 1
+# >>> b
+# 2
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ note1.py
@@ -0,0 +1,23 @@
+# print("Hallo world!!!")
+
+#用+号链接每一段话
+# s = 'Hello World!' + \
+#     'Heloo tedu'
+# print(s)
+
+#可以用help加命令查看帮助
+# help(print())
+
+# print打印多行的时候,用逗号分割,默认各项使用空格作为分割符号
+# print("hao,123")
+
+#也可以通过sep指定分隔符
+# print('Hao',123,'456')
+# print('Hao',123,'456',sep='***')
+
+#字符串中间如果需要有变化的内容,可以用$s站位,然后在字符串外边指定具体内容
+# print('I am %s' % 'zzg')
+# print('%s is %s years old. '% ('tom',20))
+
+username = input('username: f')
+print(username)
